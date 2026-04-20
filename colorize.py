01be6f0d5a63b6f93d6af3d8de51dcff43fad2a8
ayssersalhi-a11y <ayssersalhi@gmail.com>
2026-04-20 16:16:02 +0100

    Update colorize.py

--- colorize.py
+++ colorize.py
@@ -1,13 +1,15 @@
 import cv2
 import numpy as np
 import urllib.request
+import os
 
-# تحميل ملفات النموذج الجاهزةت (Deep Learning)
-prototxt_url = "https://raw.githubusercontent.com/richzhang/colorization/master/models/colorization_deploy_v2.prototxt"
-model_url = "http://eecs.berkeley.edu/~rich.zhang/projects/convolutional_colorization/models/colorization_release_v2.caffemodel"
-points_url = "https://github.com/richzhang/colorization/raw/master/resources/pts_in_hull.npy"
+# الروابط الجديدة والمضمونة للنموذج
+prototxt_url = "https://raw.githubusercontent.com/opencv/opencv_3rdparty/f930e6e5a593322b79a556396e944b25451e0655/colorization_deploy_v2.prototxt"
+model_url = "https://github.com/richzhang/colorization/raw/master/colorization/models/colorization_release_v2.caffemodel"
+points_url = "https://github.com/richzhang/colorization/raw/master/colorization/resources/pts_in_hull.npy"
 
-# تنزيل الملفات في بيئة GitHub
+# تنزيل الملفات
+print("جاري تحميل ملفات الذكاء الاصطناعي...")
 urllib.request.urlretrieve(prototxt_url, "color.prototxt")
 urllib.request.urlretrieve(model_url, "color.caffemodel")
 urllib.request.urlretrieve(points_url, "pts.npy")
@@ -16,32 +18,35 @@ urllib.request.urlretrieve(points_url, "pts.npy")
 net = cv2.dnn.readNetFromCaffe("color.prototxt", "color.caffemodel")
 pts = np.load("pts.npy")
 
-# دمج النقاط الملونة في النموذج
 class8 = net.getLayerId("class8_ab")
 conv8 = net.getLayerId("conv8_313_rh")
 net.getLayer(class8).blobs = [pts.transpose().reshape(1, 313, 1, 1).astype("float32")]
 net.getLayer(conv8).blobs = [np.full([1, 313], 2.606, dtype="float32")]
 
-# قراءة الصورة وتجهيزها
-image = cv2.imread("test.jpg")
+# تأكد من اسم الصورة التي رفعتها (يجب أن يكون test_page.jpg)
+image_path = "test_page.jpg"
+if not os.path.exists(image_path):
+    # إذا لم يجد الصورة بهذا الاسم، سيبحث عن أي صورة jpg أخرى
+    files = [f for f in os.listdir('.') if f.endswith(('.jpg', '.png', '.jpeg'))]
+    if files: image_path = files[0]
+
+print(f"جاري معالجة الصورة: {image_path}")
+image = cv2.imread(image_path)
 scaled = image.astype("float32") / 255.0
 lab = cv2.cvtColor(scaled, cv2.COLOR_BGR2LAB)
 
-# معالجة الصورة
 resized = cv2.resize(lab, (224, 224))
 L = cv2.split(resized)[0]
 L -= 50
 
 net.setInput(cv2.dnn.blobFromImage(L))
 ab = net.forward()[0, :, :, :].transpose((1, 2, 0))
-ab = cv2.resize(ab, (image.shape[1], image.shape[2]))
+ab = cv2.resize(ab, (image.shape[1], image.shape[0]))
 
-# دمج النتائج
 L = cv2.split(lab)[0]
 colorized = np.concatenate((L[:, :, np.newaxis], ab), axis=2)
 colorized = cv2.cvtColor(colorized, cv2.COLOR_LAB2BGR)
 colorized = (255 * colorized).astype("uint8")
 
-# حفظ النتيجة
 cv2.imwrite("result.jpg", colorized)
 print("تم التلوين بنجاح وحفظ الصورة باسم result.jpg")
