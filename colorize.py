7f81e248fef53919faef94f3fbe3b0836ec9bbb3
ayssersalhi-a11y <ayssersalhi@gmail.com>
2026-04-20 15:45:37 +0100

    Create colorize.py

---
+++ colorize.py
@@ -0,0 +1,47 @@
+import cv2
+import numpy as np
+import urllib.request
+
+# تحميل ملفات النموذج الجاهزة (Deep Learning)
+prototxt_url = "https://raw.githubusercontent.com/richzhang/colorization/master/models/colorization_deploy_v2.prototxt"
+model_url = "http://eecs.berkeley.edu/~rich.zhang/projects/convolutional_colorization/models/colorization_release_v2.caffemodel"
+points_url = "https://github.com/richzhang/colorization/raw/master/resources/pts_in_hull.npy"
+
+# تنزيل الملفات في بيئة GitHub
+urllib.request.urlretrieve(prototxt_url, "color.prototxt")
+urllib.request.urlretrieve(model_url, "color.caffemodel")
+urllib.request.urlretrieve(points_url, "pts.npy")
+
+# إعداد الشبكة العصبية
+net = cv2.dnn.readNetFromCaffe("color.prototxt", "color.caffemodel")
+pts = np.load("pts.npy")
+
+# دمج النقاط الملونة في النموذج
+class8 = net.getLayerId("class8_ab")
+conv8 = net.getLayerId("conv8_313_rh")
+net.getLayer(class8).blobs = [pts.transpose().reshape(1, 313, 1, 1).astype("float32")]
+net.getLayer(conv8).blobs = [np.full([1, 313], 2.606, dtype="float32")]
+
+# قراءة الصورة وتجهيزها
+image = cv2.imread("test.jpg")
+scaled = image.astype("float32") / 255.0
+lab = cv2.cvtColor(scaled, cv2.COLOR_BGR2LAB)
+
+# معالجة الصورة
+resized = cv2.resize(lab, (224, 224))
+L = cv2.split(resized)[0]
+L -= 50
+
+net.setInput(cv2.dnn.blobFromImage(L))
+ab = net.forward()[0, :, :, :].transpose((1, 2, 0))
+ab = cv2.resize(ab, (image.shape[1], image.shape[2]))
+
+# دمج النتائج
+L = cv2.split(lab)[0]
+colorized = np.concatenate((L[:, :, np.newaxis], ab), axis=2)
+colorized = cv2.cvtColor(colorized, cv2.COLOR_LAB2BGR)
+colorized = (255 * colorized).astype("uint8")
+
+# حفظ النتيجة
+cv2.imwrite("result.jpg", colorized)
+print("تم التلوين بنجاح وحفظ الصورة باسم result.jpg")
